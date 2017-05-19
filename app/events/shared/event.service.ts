22a6b47271a325eaf0b191d43e61ed8c88b2ab2d
Cezary Wójcik <cezary.wojcik@gft.com>
2017-05-19 11:51:06 +0200

    added some UT

--- app/events/shared/event.service.ts
+++ app/events/shared/event.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, EventEmitter } from '@angular/core';
-import { Subject, Observable } from 'rxjs/RX';
+import { Subject, Observable } from 'rxjs/Rx';
 import { IEvent } from './event.model';
 import { ISession } from './session.model';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
