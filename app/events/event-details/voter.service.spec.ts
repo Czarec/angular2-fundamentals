16dbea432fd728842b85de784dc9420b94294201
Cezary Wójcik <cezary.wojcik@gft.com>
2017-05-24 11:37:48 +0200

    tuning rxjs requests

--- app/events/event-details/voter.service.spec.ts
+++ app/events/event-details/voter.service.spec.ts
@@ -1,4 +1,4 @@
-import { Observable } from 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
 import { ISession } from '../shared/session.model';
 import { VoterService } from './voter.service';
 
